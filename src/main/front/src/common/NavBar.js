7aedeed566d68622d709649b78c210304ec68a8e
Raid <162943422+jsy6079@users.noreply.github.com>
2024-07-16 17:37:26 +0900

    24.7.16 메인
    24.7.16 메인[모험섬 공지사항 이벤트]

--- src/main/front/src/common/NavBar.js
+++ src/main/front/src/common/NavBar.js
@@ -3,37 +3,79 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-      <div className="container-fluid">
-        <div className="navbar-brand">
-          <span>&#x1F340;</span>Community<span>&#x1F340;</span>
-        </div>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="#">자유 게시판</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="#">구매 게시판</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="#">판매 게시판</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="#">질문과 답변</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="ml-auto">
-          <button className="btn btn-outline-light me-2" type="button" onClick={() => window.location.href='/user/login'}>로그인</button>
-          <button className="btn btn-outline-light" type="button" onClick={() => window.location.href='/user/joinMember'}>회원가입</button>
-        </div>
-      </div>
-    </nav>
-  );
+    // <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    //   <div className="container-fluid">
+    //     <div className="navbar-brand">
+    //       <span>&#x1F340;</span>Community<span>&#x1F340;</span>
+    //     </div>
+    //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="navbarNav">
+    //       <ul className="navbar-nav">
+    //         <li className="nav-item">
+    //           <Link className="nav-link active" aria-current="page" to="#">자유 게시판</Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link className="nav-link" to="#">구매 게시판</Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link className="nav-link" to="#">판매 게시판</Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link className="nav-link" to="#">질문과 답변</Link>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //     <div className="ml-auto">
+    //       <button className="btn btn-outline-light me-2" type="button" onClick={() => window.location.href='/user/login'}>로그인</button>
+    //       <button className="btn btn-outline-light" type="button" onClick={() => window.location.href='/user/joinMember'}>회원가입</button>
+    //     </div>
+    //   </div>
+    // </nav>
+
+    <header id="header" className="header d-flex align-items-center sticky-top">
+    <div className="container-fluid position-relative d-flex align-items-center justify-content-between">
+      
+        <h1>LoaMong</h1>
+
+      <nav id="navmenu" className="navmenu">
+        <ul>
+          <li><Link to="#hero">Home</Link></li>
+          <li><Link to="#about">About</Link></li>
+          <li><Link to="#services">Services</Link></li>
+          <li><Link to="#portfolio">Portfolio</Link></li>
+          <li><Link to="#team">Team</Link></li>
+          <li><Link to="blog.html">Blog</Link></li>
+          <li className="dropdown">
+            <Link to="#"><span>Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></Link>
+            <ul>
+              <li><Link to="#">Dropdown 1</Link></li>
+              <li className="dropdown">
+                <Link to="#"><span>Deep Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></Link>
+                <ul>
+                  <li><Link to="#">Deep Dropdown 1</Link></li>
+                  <li><Link to="#">Deep Dropdown 2</Link></li>
+                  <li><Link to="#">Deep Dropdown 3</Link></li>
+                  <li><Link to="#">Deep Dropdown 4</Link></li>
+                  <li><Link to="#">Deep Dropdown 5</Link></li>
+                </ul>
+              </li>
+              <li><Link to="#">Dropdown 2</Link></li>
+              <li><Link to="#">Dropdown 3</Link></li>
+              <li><Link to="#">Dropdown 4</Link></li>
+            </ul>
+          </li>
+          <li><Link to="#contact">Contact</Link></li>
+        </ul>
+        <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+      <Link className="btn-getstarted" to="/#about">Get Started</Link>
+
+    </div>
+  </header>
+);
 }
 
 export default NavBar;
--- src/main/java/com/green/controller/testController.java
+++ src/main/java/com/green/controller/testController.java
@@ -1,27 +1,19 @@
 package com.green.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
-
-
 @RestController
 @RequestMapping("/api")
+
 public class testController {
     
     @Value("${lostark.api.key}")
@@ -45,4 +37,62 @@ public class testController {
         
         return response;
     }
+    
+    @GetMapping("notice")
+    public ResponseEntity<String> getLostArkNotice() {
+        
+        String url = "https://developer-lostark.game.onstove.com/news/notices";
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("accept", "application/json");
+        headers.set("authorization", "bearer " + apiKey);  // Bearer 토큰 사용
+        
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        
+       
+        
+        return response;
+    }
+    
+    @GetMapping("characters")
+    public ResponseEntity<String> getLostArkCharacters() {
+        
+        String url = "https://developer-lostark.game.onstove.com/armories/characters/새벽이슬비o/profiles";
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("accept", "application/json");
+        headers.set("authorization", "bearer " + apiKey);  // Bearer 토큰 사용
+        
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        
+       
+        
+        return response;
+    }
+    
+    
+    @GetMapping("calender")
+    public ResponseEntity<String> getLostArkCalender() {
+        
+        String url = "https://developer-lostark.game.onstove.com/gamecontents/calendar";
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("accept", "application/json");
+        headers.set("authorization", "bearer " + apiKey);  // Bearer 토큰 사용
+        
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+               
+        return response;
+    }
+   
+
 }
