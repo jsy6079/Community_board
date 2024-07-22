8c386eecf634b8b7e68e178f19f5cef091e02c4a
jsy6079 <jsy6079@naver.com>
2024-07-22 22:58:49 +0900

    24.7.22 UI수정
    24.7.22 UI수정

--- src/main/front/src/mainSection/main.js
+++ src/main/front/src/mainSection/main.js
@@ -182,16 +182,20 @@ function Main() {
     const diffSecs = Math.floor(diffMs / 1000); // 전체 초 단위로 변환
     const hours = Math.floor(diffSecs / 3600); // 전체 시간
     const minutes = Math.floor((diffSecs % 3600) / 60); // 남은 분
-
-    if (type === 'fieldBoss') {
-      setFieldBossTimeRemaining(`${hours}시간 ${minutes}분`);
-    } else if (type === 'chaosGate') {
-      setChaosGateTimeRemaining(`${hours}시간 ${minutes}분`);
+    const seconds = diffSecs % 60; // 남은 초
+    
+    let timeString = '';
+    if (hours > 0) {
+      timeString += `${hours}시간 `;
     }
-  };
-
-
-
+    timeString += `${minutes}분 ${seconds}초`;
+    
+    if (type === 'fieldBoss') {
+      setFieldBossTimeRemaining(timeString);
+    } else if (type === 'chaosGate') {
+      setChaosGateTimeRemaining(timeString);
+    }
+  }
 
 
     // 날짜 포맷팅
@@ -213,7 +217,6 @@ function Main() {
 
       <div style={{ display: 'flex' }}>
             <div id="featured-services" className="featured-services section" style={{ maxWidth: '50%', marginRight: '5%' }}>
-                {/* 테스트용 캘린더 박스 */}
                 <h4 className="mb-1">Today Island</h4>
                 <div className="container-island">
                   <div className="row gy-4">
@@ -221,10 +224,10 @@ function Main() {
                       <div key={index} className="col-xl-4 col-md-6 d-flex" data-aos-delay="100">
                         <div className="service-item position-relative">
                           <div className="text-center">
-                            <img src={calender.ContentsIcon} alt="Contents Icon" />
+                            <img src={calender.ContentsIcon} style={{borderRadius: '5px'}} alt="Contents Icon" />
                           </div>
                           <div className="text-center">
-                            <h5 className="badge text-bg-success">{calender.ContentsName}</h5>
+                            <h5 className="badge text-bg-primary">{calender.ContentsName}</h5>
                           </div>
                           <p>
                             {calender.RewardItems.map((rewardItem, rewardIndex) =>
@@ -245,72 +248,59 @@ function Main() {
                 </div>
               </div>
 
-
-
-              
             <div id="featured-services" className="featured-services section" style={{ maxWidth: '50%'}}>
-              
+  
+             <h4 className="mb-1">Today Calenders</h4>
 
-            <h4 className="mb-1">Today Calenders</h4>
+             
               <div className="container-boss">
-                  <div className="row gy-4" >
-                    {fildBosses.list.map((fildBoss, index) => (
-                      <div key={index} className="d-flex" data-aos-delay="100">
-                        <div className="service-item d-flex align-items-center">
-                          <img style={{ width: '40px', height: '40px' }} src={fildBoss.ContentsIcon} alt="Field Boss Icon" />
-                          <span className="text-label">{fieldBossTimeRemaining}</span>
-                        </div>
+                <div className="row gy-4" style={{ width: '100%', height: '225px', overflowY: 'auto' }}>
+                
+                  {chaosGates.list.slice(0, 3).map((chaosGate, index) => (
+                    <div key={index} className="d-flex" data-aos-delay="100">
+                      <div className="d-flex align-items-center">
+                        <img style={{ width: '40px', height: '40px', borderRadius: '10px', marginRight: '10px'}} src={chaosGate.ContentsIcon} alt="Chaos Gate Icon" />
+                        <span
+                          className="text-label"
+                          style={{
+                            color: '#735bd2', // 폰트 색상
+                            fontSize: '20px', // 폰트 크기
+                            fontWeight: 'bold', // 폰트 두께
+                            fontFamily: 'Arial, sans-serif', // 폰트 패밀리
+                          }}
+                        >
+                          {chaosGateTimeRemaining}
+                        </span>
                       </div>
-                    ))}
-                    {chaosGates.list.map((chaosGate, index) => (
-                        <div key={index} className="d-flex" data-aos-delay="100">
-                          <div className="service-item d-flex align-items-center">
-                            <img style={{ width: '40px', height: '40px' }} src={chaosGate.ContentsIcon} alt="Field Boss Icon" />
-                            <span className="text-label">{chaosGateTimeRemaining}</span>
-                          </div>
-                        </div>
-                      ))}
-                  </div>
-              </div>
-
-              {/* <div className="container-boss">
-                  <div className="row gy-4">
-                    {chaosGates.list.map((chaosGate, index) => (
-                      <div key={index} className="d-flex" data-aos-delay="100">
-                        <div className="service-item d-flex align-items-center">
-                          <img style={{ width: '40px', height: '40px' }} src={chaosGate.ContentsIcon} alt="Field Boss Icon" />
-                          <span className="text-label">{chaosGateTimeRemaining}</span>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-              </div> */}
-
-
-              {/* <div className="container-boss">
-                <div className="row gy-4">
-                  {chaosGates.list.map((chaosGate, index) => (
-                    <div key={index} className="col-xl-3 col-md-6 d-flex" data-aos-delay="100">
-                      <div className="service-item position-relative">
-                        <div className="text-center">
-                          <img style={{ width: '40px', height: '40px' }} src={chaosGate.ContentsIcon} alt="Filed Boss Icon" />
-                        </div>
-                        <div>
-                        <div>{chaosGateTimeRemaining}</div>
-                       
-                      </div>
+                    </div>
+                ))}
+                  {fildBosses.list.slice(0, 3).map((fildBoss, index) => (
+                    <div key={index} className="d-flex" data-aos-delay="100">
+                      <div className="d-flex align-items-center">
+                        <img style={{ width: '40px', height: '40px', borderRadius: '10px', marginRight: '10px'}} src={fildBoss.ContentsIcon} alt="Field Boss Icon" />
+                        <span
+                          className="text-label"
+                          style={{
+                            color: '#735bd2', // 폰트 색상
+                            fontSize: '20px', // 폰트 크기
+                            fontWeight: 'bold', // 폰트 두께
+                            fontFamily: 'Arial, sans-serif', // 폰트 패밀리
+                          }}
+                        >
+                          {fieldBossTimeRemaining}
+                        </span>
                       </div>
                     </div>
                   ))}
                 </div>
-              </div> */}
+            </div>
+
+
+
             </div>
 
 
     </div>
-
-
-
 
           <div className="container mt-60">
                 <div className="row align-items-center mb-4 pb-2">
